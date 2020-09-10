55887ab5941651ba90b6c2cff3673656d9a26189
Guandong Kou <gdkou90@gmail.com>
2020-09-10 10:38:33 -0400

    HW1 part1 done

--- Assignment1/PartI/MyInitials.java
+++ Assignment1/PartI/MyInitials.java
@@ -2,6 +2,16 @@
 public class MyInitials {
 
 	public static void main(String[] args) {
-
+		// My name is Guandong Kou, and thus my intials should be GK.
+		System.out.println();
+		System.out.println("       GGG      K   K      ");
+		System.out.println("      G   G     K  K       ");
+		System.out.println("      G         K K        ");
+		System.out.println("      GGGGG     KK         ");
+		System.out.println("      G   G     K K        ");
+		System.out.println("      G   G     K  K       ");
+		System.out.println("       GGG      K   K      ");
+		System.out.println();
 	}
+
 }
--- Assignment1/PartI/PrintResults.java
+++ Assignment1/PartI/PrintResults.java
@@ -2,13 +2,21 @@
 public class PrintResults {
 
 	public static void main(String[] args) {
-		
-		System.out.println("The value of 1+1 is " + (1+1));
+
+		System.out.println("The value of 1+1 is " + (1 + 1));
 		int a = 10 + 5;
 		int b = 50 - 23;
 		int c = 12 * 13;
 		double d = 20.0 / 3.0;
 		int e = 100 % 7;
 		double f = Math.pow(4, 3);
+		// the value of a is [Value]
+		System.out.format("The value of a is [%s]\n", a);
+		System.out.format("The value of b is [%s]\n", b);
+		System.out.format("The value of c is [%s]\n", c);
+		System.out.format("The value of d is [%s]\n", d);
+		System.out.format("The value of e is [%s]\n", e);
+		System.out.format("The value of f is [%s]\n", f);
+		System.out.println();
 	}
 }

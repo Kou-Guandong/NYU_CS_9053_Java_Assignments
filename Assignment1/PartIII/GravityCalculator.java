abccb59e3ce58ca3474419de866eb3397e7bc740
Guandong Kou <gdkou90@gmail.com>
2020-09-10 14:19:51 -0400

    hw1 done

--- Assignment1/PartIII/GravityCalculator.java
+++ Assignment1/PartIII/GravityCalculator.java
@@ -1,13 +1,12 @@
 class GravityCalculator {
 
-	
 	public static void main(String[] arguments) {
 		double gravity = -9.81; // Earth's gravity in m/s^2
 		double initialVelocity = 0.0;
 		double fallingTime = 10.0;
 		double initialPosition = 0.0;
-		double finalPosition = 0.0;
-		 System.out.println("The object's position after " + fallingTime +
-		" seconds is " + finalPosition + " m.");
+		double finalPosition = 0.5 * gravity * Math.pow(fallingTime, 2) + initialVelocity * fallingTime
+				+ initialPosition;
+		System.out.println("The object's position after " + fallingTime + " seconds is " + finalPosition + " m.");
 	}
 }

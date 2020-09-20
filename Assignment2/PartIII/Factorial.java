3f5e2e857083775133b5f8ce1d99ff4a4f5faa04
Guandong Kou <gdkou90@gmail.com>
2020-09-20 11:30:13 -0400

    hw2 p3 2 done

--- Assignment2/PartIII/Factorial.java
+++ Assignment2/PartIII/Factorial.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
 
 public class Factorial {
 
 	public static void main(String[] args) {
-		int val = 0;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter the value n: ");
+		int n = input.nextInt();
+		int val = n;
+		input.close();
+		int result = 1;
+		while (val > 1) {
+			result *= val;
+			val -= 1;
+		}
+		System.out.format("The factorial of %d is: %d\n", n, result);
 	}
 }
--- Assignment2/PartIII/LoopSum.java
+++ Assignment2/PartIII/LoopSum.java
@@ -2,17 +2,29 @@
 public class LoopSum {
 
 	public static int sum100() {
-	
-		return -1;
+		int result = 0;
+		int value = 1;
+		while (value <= 100){
+			result += value;
+			value++;
+		}
+		return result;
 	}
 	
-	public static int sumN() {
-		
-		return -1;
+	public static int sumN(int n) {
+		int result = 0;
+		int value = 1;
+		while (value <= n){
+			result += value;
+			value++;
+		}
+		return result;
 	}
 	
 	public static void main(String[] args) {
-		
+		System.out.println(sum100());
+		System.out.println(sumN(100));
+		System.out.println(sumN(10));
 	}
 	
 }

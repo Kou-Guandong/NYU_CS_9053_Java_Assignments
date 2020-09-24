75b89742f5f2f0fbe82627d433149e65f6d7de60
Guandong Kou <gdkou90@gmail.com>
2020-09-24 11:33:46 -0400

    added hw3 folder

---
+++ Assignment3/PartI/Holiday.java
@@ -0,0 +1,13 @@
+
+public class Holiday {
+	private String name;
+	private int day;
+	private String month;
+	
+	public static void main(String[] args) {
+		
+		Holiday[] holidays = new Holiday[5];
+		holidays[0] = new Holiday("May Day", 1, "May");
+		
+	}
+}

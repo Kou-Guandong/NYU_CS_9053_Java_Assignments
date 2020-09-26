978e7e77b537b7caffa283e8c2cac333e5d4b0cb
Guandong Kou <gdkou90@gmail.com>
2020-09-26 11:43:42 -0400

    hw3 ellipse

--- Assignment3/PartI/Ellipse.java
+++ Assignment3/PartI/Ellipse.java
@@ -1,4 +1,34 @@
 
 public class Ellipse {
-	
+    private static int nextId;
+    private int id = ++nextId;
+
+    private double semiMajorAxis = 2.0;
+    private double semiMinorAxis = 1.0;
+
+    public Ellipse() {
+
+    }
+
+    public Ellipse(double a, double b) {
+        this.semiMajorAxis = a;
+        this.semiMinorAxis = b;
+    }
+
+    public double getSemiMajorAxis() {
+        return this.semiMajorAxis;
+    }
+
+    public double getSemiMinorAxis() {
+        return this.semiMinorAxis;
+    }
+
+    public double getArea() {
+        return this.semiMajorAxis * this.semiMinorAxis * Math.PI;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
 }

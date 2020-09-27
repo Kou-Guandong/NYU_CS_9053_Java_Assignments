2d6cdd5cddac2699778a26251409790e69a1e1c9
Guandong Kou <gdkou90@gmail.com>
2020-09-26 21:00:26 -0400

    hw3 done

--- Assignment3/PartII/Account.java
+++ Assignment3/PartII/Account.java
@@ -1,4 +1,36 @@
- 
+
 public class Account {
+    private static int nextId = 0;
+    private int id = ++nextId;
+    private double balance;
+
+    public Account() {
+
+    }
+
+    public Account(double startingBalance) {
+        this.balance = startingBalance;
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount <= this.balance) {
+            this.balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public int getId() {
+        return this.id;
+    }
 
 }
--- Assignment3/PartII/Bank.java
+++ Assignment3/PartII/Bank.java
@@ -1,43 +1,54 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class Bank {
 
 	public static final int NUM_ACCOUNTS = 5;
-	
-	
+	private Account[] accounts;
+
 	public Bank() {
 		// create NUM_ACCOUNTS accounts in an array
 		// with each account having a starting balance
 		// of $1000
+		this.accounts = new Account[Bank.NUM_ACCOUNTS];
+		for (int i = 0; i < NUM_ACCOUNTS; i++) {
+			this.accounts[i] = new Account(1000);
+		}
 	}
-	
+
 	public Account getAccountById(int id) {
-		
 		// this should take an id, search
 		// through the array of accounts until
 		// it finds the account matching the id
 		// and returns it, returning null if
 		// it is not found
-		
-		return null;
+		Optional<Account> result = Arrays.stream(this.accounts).filter(x -> x.getId() == id).findAny();
+		if (result.isPresent()) {
+			return result.get();
+		} else {
+			return null;
+		}
 	}
-	
+
 	public int numAccounts() {
 		// returns the number of accounts:
 		// ie, the length of the array accounts
-		return 0;
+		return this.accounts.length;
 	}
-	
+
+	public Account[] getAllAccounts() {
+		return this.accounts;
+	}
+
 	public static void main(String[] args) {
-		
+
 		Scanner in = new Scanner(System.in);
 		Bank bank = new Bank();
 		for (;;) {
 			System.out.println("Accounts:");
 			Account[] accounts = bank.getAllAccounts();
-			for (int i=0;i< accounts.length;i++) {
-				System.out.println("Account " + accounts[i].getId() + 
-						": $" + accounts[i].getBalance());
+			for (int i = 0; i < accounts.length; i++) {
+				System.out.println("Account " + accounts[i].getId() + ": $" + accounts[i].getBalance());
 			}
 			System.out.print("Enter source account: ");
 			int srcAccountId = in.nextInt();

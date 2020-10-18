d1120bdce36183e328c81770fd45ebe1d5a8271d
Guandong Kou <gdkou90@gmail.com>
2020-10-17 21:04:37 -0400

    init midterm

---
+++ Midterm/Question1/edu/nyu/cs9053/midterm/hierarchy/WinterSportPlayer.java
@@ -0,0 +1,14 @@
+package edu.nyu.cs9053.midterm.hierarchy;
+
+public abstract class WinterSportPlayer {
+
+	public String getName() {
+		
+		return null;
+	}
+	
+	public int getAge() {
+		
+		return -1;
+	}
+}

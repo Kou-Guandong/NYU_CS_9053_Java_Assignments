a5f2e095df7b4d5b2fb775c0f1129bf35b442cf8
Guandong Kou <gdkou90@gmail.com>
2020-11-03 23:47:29 -0500

    hw7 done

--- Assignment7/PartI/WordFinder.java
+++ Assignment7/PartI/WordFinder.java
@@ -1,62 +1,93 @@
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import javax.swing.*;
 import java.util.List;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.DefaultListModel;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JList;
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import javax.swing.event.DocumentListener;
+import javax.swing.event.DocumentEvent;
 
 public class WordFinder extends JFrame {
 
+	private static final long serialVersionUID = 1773693320346723944L;
 	JFileChooser jFileChooser;
 	private JPanel topPanel; // the top line of objects is going to go here
 	WordList wordList;
-	JList list;
-	private JTextArea wordsBox; // results of the search go in heer.
-	
+	private JTextField queryField;
+	private JTextArea wordsBox = new JTextArea(16, 42); // results of the search go in heer.
+
+	class ClearListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			queryField.setText("");
+		}
+	}
 
 	public WordFinder() {
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		// set the size correctly
-		this.setSize(500, 300);
+		this.setSize(600, 400);
 		jFileChooser = new JFileChooser(".");
 		wordList = new WordList();
 
 		JPanel panelForTextFields = new JPanel();
-		panelForTextFields.setSize(400, 180);
+		panelForTextFields.setSize(300, 320);
 
+		// Construct menu
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		menuBar.add(createMenus());
 
-		//panel at the top
+		// panel at the top
 		topPanel = new JPanel();
-		
-		// there should be objects in the top panel
-		
-		// There should probably be something passed into the JScrollPane
 
-		model = new DefaultListModel();
-		list = new JList(model);
+		// there should be objects in the top panel
+		queryField = new JTextField(15);
+		JButton clearButton = new JButton("Clear");
+
+		topPanel.add(new JLabel("Find: ")); // input prompt text
+		topPanel.add(queryField);
+		topPanel.add(clearButton);
+
+		// There should probably be something passed into the JScrollPane
+		wordsBox.setEditable(false);
+		JScrollPane scrollPane = new JScrollPane(wordsBox);
+		panelForTextFields.add(scrollPane);
 
 		// and of course you will want them to be properly aligned in the frame
-		
+		add(topPanel, BorderLayout.NORTH);
+		add(panelForTextFields);
+
+		ActionListener clearListener = new ClearListener();
+		clearButton.addActionListener(clearListener);
+
+		class QueryListener implements DocumentListener {
+			public void changedUpdate(DocumentEvent e) {
+				queryAndUpdate(panelForTextFields);
+			}
+
+			public void removeUpdate(DocumentEvent e) {
+				queryAndUpdate(panelForTextFields);
+			}
+
+			public void insertUpdate(DocumentEvent e) {
+				queryAndUpdate(panelForTextFields);
+			}
+		}
+
+		queryField.getDocument().addDocumentListener(new QueryListener());
 
 	}
-	
-	private void createMenus() {
-		/* add a "File" menu with:
-		 * "Open" item which allows you to choose a new file
-		 * "Exit" item which ends the process with System.exit(0);
-		 * Key shortcuts are optional
+
+	private JMenu createMenus() {
+		/*
+		 * add a "File" menu with: "Open" item which allows you to choose a new file
+		 * "Exit" item which ends the process with System.exit(0); Key shortcuts are
+		 * optional
 		 */
-		
+
 		/* OpenActionListener that will open the file chooser */
 		class OpenActionListener implements ActionListener {
 			public void actionPerformed(ActionEvent e) {
@@ -64,37 +95,64 @@ public class WordFinder extends JFrame {
 				myFileListener.actionPerformed(e);
 			}
 		}
-		
+
+		class ExitActionListener implements ActionListener {
+			public void actionPerformed(ActionEvent event) {
+				System.exit(0);
+			}
+		}
+
+		JMenu menu = new JMenu("File");
+		JMenuItem MenuItemExit = new JMenuItem("Exit");
+		JMenuItem menuItemOpen = new JMenuItem("Open");
+
+		MenuItemExit.addActionListener(new ExitActionListener());
+		menuItemOpen.addActionListener(new OpenActionListener());
+
+		menu.add(menuItemOpen);
+		menu.add(MenuItemExit);
+		return menu;
 	}
 
-	private void find(){
-		List searchResult = null; // figure out from WordList how to get this
-
+	private void getQueryResult() {
+		String query = queryField.getText();
+		List<String> searchResult = wordList.find(query); // figure out from WordList how to get this
 		// you're going to want to get the words in the
 		// searchResult list and put them in the textbox.
+		wordsBox.setText("");
+		for (Object word : searchResult) {
+			wordsBox.append(word + "\n");
+		}
+		wordsBox.setCaretPosition(0);
 	}
-	
+
+	private void queryAndUpdate(JPanel panel2Repaint) {
+		// same as getQueryResult with no argument, but also repaint the input JPanel
+		// after result queried.
+		getQueryResult();
+		panel2Repaint.repaint();
+	}
+
 	class OpenFileListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			int returnVal = jFileChooser.showOpenDialog(getParent());
 			if (returnVal == JFileChooser.APPROVE_OPTION) {
 				try {
-					System.out.println("You chose to open this file: " + jFileChooser.getSelectedFile().getAbsolutePath());
+					System.out.println(
+							"You chose to open this file: " + jFileChooser.getSelectedFile().getAbsolutePath());
 
 					InputStream in = new FileInputStream(jFileChooser.getSelectedFile().getAbsolutePath());
 					wordList.load(in);
 
-					List searchResult = null; // figure out from WordList how to get this
-					// because you will load in a wordlist and there
-					// might be data in the search box
+					// invode find method to get query result
+					getQueryResult();
 
-				} catch (IOException error){
+				} catch (IOException error) {
 					error.printStackTrace();
 				}
 			}
 		}
 	}
-
 
 	public static void main(String[] args) {
 
--- Assignment7/PartI/WordList.java
+++ Assignment7/PartI/WordList.java
@@ -4,84 +4,86 @@ import java.text.Collator;
 import java.util.*;
 
 /**
- * A WordList is a set of words loaded from a file and searchable by
- * substring.  A word is defined as a sequence of letters
- * (upper case or lower case).  WordSets ignore alphabetic case when
- * comparing, searching, or sorting. 
+ * A WordList is a set of words loaded from a file and searchable by substring.
+ * A word is defined as a sequence of letters (upper case or lower case).
+ * WordSets ignore alphabetic case when comparing, searching, or sorting.
  */
 public class WordList {
-    private List words;
-    
+    // private List<> words;
+    private List<String> words;
+
     /*
-     * Rep invariant:
-     *      words != null
+     * Rep invariant: words != null
      */
-    
+
     /**
      * Make an empty WordList.
      */
     public WordList() {
     }
 
-    public List getWords() {
+    public List<String> getWords() {
         return words;
     }
 
     /**
      * Load a file into this word set.
-     * @effects Removes all the words from this word set and replaces 
-     * them with the words found in the given file.  
+     * 
+     * @effects Removes all the words from this word set and replaces them with the
+     *          words found in the given file.
      * @param filename File to load
      * @throws IOException if file cannot be opened
      */
     public void load(InputStream in) throws IOException {
         Collator c = Collator.getInstance();
         c.setStrength(Collator.PRIMARY);
-        Set s = new TreeSet(c);
-        
+        Set<String> s = new TreeSet<String>(c);
+
         StreamTokenizer tok = new StreamTokenizer(new InputStreamReader(in));
         tok.resetSyntax();
         tok.wordChars('a', 'z');
         tok.wordChars('A', 'Z');
         tok.wordChars('\'', '\'');
-        
+
         while (tok.nextToken() != StreamTokenizer.TT_EOF) {
             if (tok.ttype == StreamTokenizer.TT_WORD)
                 s.add(tok.sval);
         }
-        
-        words = new ArrayList(s);
+
+        words = new ArrayList<String>(s);
     }
-    
+
     /**
      * Find words containing a given substring.
+     * 
      * @param s Substring to search for
-     * @return list of words in this word set (sorted case-insensitively)
-     * that contain the substring s (matched case-insensitively).  A
-     * word appears at most once in the returned list.
+     * @return list of words in this word set (sorted case-insensitively) that
+     *         contain the substring s (matched case-insensitively). A word appears
+     *         at most once in the returned list.
      */
-    public List find(String s) {
+    public List<String> find(String s) {
         if (s.length() == 0) {
             return Collections.unmodifiableList(words);
         }
-        
+
         s = s.toLowerCase();
-        List l = new ArrayList();
-        for (Iterator i = words.iterator(); i.hasNext(); ) {
+        List<String> l = new ArrayList<String>();
+        for (Iterator<String> i = words.iterator(); i.hasNext();) {
             String word = (String) i.next();
             if (word.toLowerCase().indexOf(s) != -1)
                 l.add(word);
         }
         return l;
     }
-    
+
     /**
-     * Main method.  Demonstrates how to use this class.
-     * @param args Command-line arguments.  Ignored.
+     * Main method. Demonstrates how to use this class.
+     * 
+     * @param args Command-line arguments. Ignored.
      */
     public static void main(String[] args) {
-        WordList words = new WordList ();
-        
+        WordList words = new WordList();
+
         // Create the word list from a resource.
         // The words file must be in the same directory (or jar file directory)
         // as this class.
@@ -93,11 +95,11 @@ public class WordList {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        
+
         // Print all the words containing "ph"
-        List matches = words.find("holst");
-        for (Iterator i = matches.iterator(); i.hasNext(); ) {
+        List<String> matches = words.find("holst");
+        for (Iterator<String> i = matches.iterator(); i.hasNext();) {
             System.out.println(i.next());
         }
-    }    
+    }
 }

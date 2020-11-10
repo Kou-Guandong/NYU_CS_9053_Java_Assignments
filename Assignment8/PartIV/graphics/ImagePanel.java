61816a5eb65d9ff0c8e4af4541bbd51f2d9f24a9
Guandong Kou <gdkou90@gmail.com>
2020-11-10 06:18:33 -0500

    p4 in progress

--- Assignment8/PartIV/graphics/ImagePanel.java
+++ Assignment8/PartIV/graphics/ImagePanel.java
@@ -1,27 +1,36 @@
 package graphics;
 
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Image;
+import javax.swing.*;
+import java.io.*;
+// import java.awt.Dimension;
+// import java.awt.Graphics;
+import java.awt.*;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class ImagePanel extends JPanel {
-	private Image img;
-	
-	public ImagePanel(String img) {
-		this(new ImageIcon(img).getImage());
-	}
-	
-	public ImagePanel(Image img) {
-		this.img = img;
+    private static final long serialVersionUID = 399074610321541873L;
+    private Image img;
+
+    public ImagePanel(String img) {
+        this(new ImageIcon(img).getImage());
+    }
+
+    public ImagePanel(Image img) {
+        this.img = img;
         Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+        // setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600, 400);
         setPreferredSize(size);
-        /*setMinimumSize(size);
-        setMaximumSize(size);
-        setSize(size);*/
+        /*
+         * setMinimumSize(size); setMaximumSize(size); setSize(size);
+         */
         setLayout(null);
-	}
+        add(topPanel, BorderLayout.NORTH);
+
+
+    }
+
     public void paintComponent(Graphics g) {
         g.drawImage(img, 50, 0, null);
     }
--- Assignment8/PartIV/graphics/RollDice.java
+++ Assignment8/PartIV/graphics/RollDice.java
@@ -1,6 +1,7 @@
 package graphics;
 
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class RollDice extends JFrame {
 
@@ -10,6 +11,9 @@ public class RollDice extends JFrame {
 
 	public static void main(String[] args) {
 		RollDice rollDice = new RollDice();
+
+		ImagePanel panel = new ImagePanel("die.png");
+		panel.setVisible(true);
 	
 	}
 }

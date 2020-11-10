8b0251ee0397cb34886de68b11a12224258c83d6
Guandong Kou <gdkou90@gmail.com>
2020-11-09 20:15:10 -0500

    p3 done

--- Assignment8/PartIII/PartIII/ReverseMap.java
+++ Assignment8/PartIII/PartIII/ReverseMap.java
@@ -1,17 +1,27 @@
+package PartIII;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 
 public class ReverseMap {
 
 	public static Map<Object, Set<Object>> getInverted(Map<Object, Object> mp) {
-		
-
+		Map<Object, Set<Object>> result = new HashMap<Object, Set<Object>>();
+		for (Map.Entry<Object, Object> entry : mp.entrySet()) {
+			Object key = entry.getKey();
+			Object value = entry.getValue();
+			if (!result.containsKey(value)) {
+				result.put(value, new HashSet<Object>());
+			}
+			result.get(value).add(key);
+		}
+		return result;
 	}
+
 	public static void main(String[] args) {
-		Map<Object,Object> m = new HashMap<Object,Object>();
+		Map<Object, Object> m = new HashMap<Object, Object>();
 		m.put("Key1", new Integer(2));
 		m.put("Key2", new Integer(5));
 		m.put("Key4", new Integer(2));
@@ -19,9 +29,8 @@ public class ReverseMap {
 		m.put("Key6", new Integer(18));
 		m.put("Key7", new Integer(24));
 		System.out.println("hashmap is " + m);
-		
+
 		System.out.println("inverted: " + ReverseMap.getInverted(m));
-		
 
 	}
 
